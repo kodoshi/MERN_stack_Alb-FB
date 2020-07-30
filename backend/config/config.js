93711f4e976a1f73c7590075d39520adc6e61940
kodoshi <61183170+kodoshi@users.noreply.github.com>
2020-07-31 00:21:19 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,4 @@
 exports.dbCFG = "mongodb://localhost:27017/codac";
 process.env.PORT = 4242;
 process.env.NODE_ENV = "development";
-exports.JWT_SECRET = "3ce@K8bfp:N4CHK}K3<K}y@e}47]N";
-
-//global.db_cfg = dbCFG;
+exports.JWT_SECRET = "b1gsecrethere";
